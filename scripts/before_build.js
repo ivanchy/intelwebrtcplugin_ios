143c172e30d632ddedc4376ec9df10a75d48a3c5
Ivan Chiu <ivan.chy@hotmail.com>
2022-11-08 15:12:00 +0800

    b

--- scripts/before_build.js
+++ scripts/before_build.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const util = require('util');
 const getSize = require('get-folder-size');
-//const stat = util.promisify(fs.stat);
+const stat = util.promisify(fs.stat);
 var path = require('path');
 
 module.exports = function(ctx) {
@@ -34,17 +34,27 @@ module.exports = function(ctx) {
     
     console.log("current path : "+ path.join('./platforms/ios/intelwebrtc_ios'));
     
-    getSize('./platforms/ios/intelwebrtc_ios/Plugins/owt.sample.conference.intelwebrtcplugin/OWT.framework', (err, size) => {
-        if (err) { throw err; }
-          
-          console.log(`The folder is ${size} bytes large`);
-          console.log(`That is the same as ${(size / 1000 / 1000).toFixed(2)} MB`);
-      });
+//    getSize('./platforms/ios/intelwebrtc_ios/Plugins/owt.sample.conference.intelwebrtcplugin/OWT.framework/OWT', (err, size) => {
+//        if (err) { throw err; }
+//
+//          console.log(`The folder is ${size} bytes large`);
+//          console.log(`That is the same as ${(size / 1000 / 1000).toFixed(2)} MB`);
+//      });
     
-    getSize('./platforms/ios/intelwebrtc_ios/Plugins/owt.sample.conference.intelwebrtcplugin/WebRTC.framework', (err, size) => {
-        if (err) { throw err; }
-          
-          console.log(`The folder is ${size} bytes large`);
-          console.log(`That is the same as ${(size / 1000 / 1000).toFixed(2)} MB`);
-      });
+    stat('./platforms/ios/intelwebrtc_ios/Plugins/owt.sample.conference.intelwebrtcplugin/OWT.framework/OWT').then(stats => {
+          console.log(`Size of OWT.framework is ${stats.size} bytes`);
+        });
+    
+//    getSize('./platforms/ios/intelwebrtc_ios/Plugins/owt.sample.conference.intelwebrtcplugin/WebRTC.framework/WebRTC', (err, size) => {
+//        if (err) { throw err; }
+//
+//          console.log(`The folder is ${size} bytes large`);
+//          console.log(`That is the same as ${(size / 1000 / 1000).toFixed(2)} MB`);
+//      });
+    
+    stat('./platforms/ios/intelwebrtc_ios/Plugins/owt.sample.conference.intelwebrtcplugin/WebRTC.framework/WebRTC').then(stats => {
+          console.log(`Size of WebRTC.framework is ${stats.size} bytes`);
+        });
+    
+    
 };
