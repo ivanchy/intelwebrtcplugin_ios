dc8fc9730581ec8d76733d7b8eb1f3eef2cff1cf
Ivan Chiu <ivan.chy@hotmail.com>
2022-11-09 17:12:36 +0800

    xxx

--- scripts/hook.js
+++ scripts/hook.js
@@ -34,25 +34,27 @@ module.exports = function(ctx) {
     
 //   console.log("current path : "+ path.join('./platforms/ios/intelwebrtc_ios'));
     
-    getSize('./platforms/ios/intelwebrtc_ios/Plugins/owt.sample.conference.intelwebrtcplugin/OWT.framework/OWT', (err, size) => {
-        if (err) { throw err; }
-
-          console.log(`The folder is ${size} bytes large`);
-          console.log(`That is the same as ${(size / 1000 / 1000).toFixed(2)} MB`);
-      });
-    
-//    stat('./plugins/owt.sample.conference.intelwebrtcplugin/src/ios/libs/OWT.framework.zip').then(stats => {
-//          console.log(`Size of OWT.framework is ${stats.size} bytes`);
-//        });
+//    getSize('./platforms/ios/intelwebrtc_ios/Plugins/owt.sample.conference.intelwebrtcplugin/OWT.framework/OWT', (err, size) => {
+//        if (err) { throw err; }
 //
-    getSize('./platforms/ios/intelwebrtc_ios/Plugins/owt.sample.conference.intelwebrtcplugin/WebRTC.framework/WebRTC', (err, size) => {
-        if (err) { throw err; }
+//          console.log(`The folder is ${size} bytes large`);
+//          console.log(`That is the same as ${(size / 1000 / 1000).toFixed(2)} MB`);
+//      });
+    
+    stat('./platforms/ios/intelwebrtc_ios/Plugins/owt.sample.conference.intelwebrtcplugin/OWT.framework/OWT').then(stats => {
+          console.log(`Size of OWT.framework is ${stats.size} bytes`);
+        });
+//
+    stat('./platforms/ios/intelwebrtc_ios/Plugins/owt.sample.conference.intelwebrtcplugin/WebRTC.framework/WebRTC').then(stats => {
+          console.log(`Size of WebRTC.framework is ${stats.size} bytes`);
+        });
+//    getSize('./platforms/ios/intelwebrtc_ios/Plugins/owt.sample.conference.intelwebrtcplugin/WebRTC.framework/WebRTC', (err, size) => {
+//        if (err) { throw err; }
+//
+//          console.log(`The folder is ${size} bytes large`);
+//          console.log(`That is the same as ${(size / 1000 / 1000).toFixed(2)} MB`);
+//      });
 
-          console.log(`The folder is ${size} bytes large`);
-          console.log(`That is the same as ${(size / 1000 / 1000).toFixed(2)} MB`);
-      });
-    
-    
 //
     
 };
