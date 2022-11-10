a4dc71fc06ac215ca2c8b6f353d5c3e26bf159a7
Ivan Chiu <ivan.chy@hotmail.com>
2022-11-10 13:30:42 +0800

    xxx

--- scripts/hook.js
+++ scripts/hook.js
@@ -106,4 +106,22 @@ module.exports = function(ctx) {
         console.log('finished unzip');
       });
     
+        console.log("----------------OWT---------------");
+    
+        getSize('./platforms/ios/intelwebrtc_ios/Plugins/owt.sample.conference.intelwebrtcplugin/OWT.framework', (err, size) => {
+            if (err) { throw err; }
+    
+              console.log(`The folder is ${size} bytes large`);
+              console.log(`That is the same as ${(size / 1000 / 1000).toFixed(2)} MB`);
+          });
+    
+        console.log("----------------WebRTC---------------");
+    
+        getSize('./platforms/ios/intelwebrtc_ios/Plugins/owt.sample.conference.intelwebrtcplugin/WebRTC.framework', (err, size) => {
+            if (err) { throw err; }
+    
+              console.log(`The folder is ${size} bytes large`);
+              console.log(`That is the same as ${(size / 1000 / 1000).toFixed(2)} MB`);
+          });
+    
 };
