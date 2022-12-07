fa6af00667d4dc2c6377842776cc6b4929d90164
Ivan Chiu <ivan.hy.chiu@pccw.com>
2022-12-07 19:41:17 +0800

    xxx

--- scripts/embed_framework_hook.js
+++ scripts/embed_framework_hook.js
@@ -24,7 +24,7 @@ module.exports = function (ctx) {
 
     
     const platformRoot = path.join(ctx.opts.projectRoot, 'platforms/ios');
-    const apkFileLocation = path.join(platformRoot, 'intelwebrtc_ios_new/Plugins/owt.sample.conference.intelwebrtcplugin');
+    const apkFileLocation = path.join(platformRoot, 'TheCloud_Clone_Ivan/Plugins/owt.sample.conference.intelwebrtcplugin');
     //var fs = ctx.requireCordovaModule("fs");
     //var path = ctx.requireCordovaModule("path");
     //var xcode = ctx.requireCordovaModule("xcode");
